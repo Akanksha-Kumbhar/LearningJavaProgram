38a94b89cc7450ccaab46f859b8c168637581594
Akanksha Kumbhar <Akankshakumbhar22@gmail.com>
2025-05-19 00:12:54 +0530

    18th May 2025

---
+++ src/ex_02_Java_Basics/Lab09_printF.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab09_printF {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.printf("a = %d, b = %d", a,b);
+        System.out.println();
+        System.out.printf("Value of a is = %d, Value of b is = %d", a,b);
+        // %d = int, short, byte, long--data type
+        // %s = string
+        // %f = float, double
+        // %b = boolean
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab10_Task_Table_of_5.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab10_Task_Table_of_5 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("%d * %d = %d",num,1,num*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,2,num*2);
+        System.out.println();
+        System.out.println("---------------");
+        System.out.printf("%d * %d =%d",num,10,num*10);
+    }
+}
